098931abc93063b3189218372e22110f7d009020
danismithu <54519598+danismithu@users.noreply.github.com>
2019-10-23 23:34:53 -0600

    Add files via upload

---
+++ static/analyze.js
@@ -0,0 +1,37 @@
+var el = x => document.getElementById(x);
+
+function showPicker() {
+  el("file-input").click();
+}
+
+function showPicked(input) {
+  el("upload-label").innerHTML = input.files[0].name;
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    el("image-picked").src = e.target.result;
+    el("image-picked").className = "";
+  };
+  reader.readAsDataURL(input.files[0]);
+}
+
+function predict() {
+  var form_data = new FormData($('#upload-file')[0]);
+
+  el("analyze-button").innerHTML = "Analyzing...";
+
+  $.ajax({
+    type: 'POST',
+    url: '/predict',
+    data: form_data,
+    contentType: false,
+    processData: false,
+    async: true,
+    success: function(data) {
+      el("result-label").innerHTML = 'Prediction is: ' + data;
+      el("analyze-button").style.visibility = 'hidden';
+      el("upload-button").style.visibility = 'hidden';
+      el("analyze-button").disabled = false;
+      el("upload-button").disabled = false;
+    },
+  });
+}
